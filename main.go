27fb2f331ae02fe1869c2b0f527e78a7c6cb1e82
Tharak5959 <141053852+Tharak5959@users.noreply.github.com>
2025-06-04 16:34:41 +0530

    final

--- main.go
+++ main.go
@@ -41,7 +41,7 @@ func init() {
     ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
     defer cancel()
 
-    client, err := mongo.Connect(ctx, options.Client().ApplyURI("your-mongodb-uri"))
+    client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017/"))
     if err != nil {
         log.Fatalf("Failed to connect to MongoDB: %v", err)
     }
